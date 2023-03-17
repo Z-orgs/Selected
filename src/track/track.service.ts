7b33c629de0505864c50a2f3bd4672bfdd6777d5
mxz <60683892+mxzxm@users.noreply.github.com>
2023-03-18 06:39:04 +0700

    Update track.service.ts

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,24 +1,17 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { InjectConnection, InjectModel } from '@nestjs/mongoose';
-import { Response } from 'express';
+import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
 import { MongoGridFS } from 'mongo-gridfs';
-import { GridFSBucket, ObjectId } from 'mongodb';
-import mongoose, { Connection } from 'mongoose';
-import { async } from 'rxjs/internal/scheduler/async';
-import { Socket } from 'socket.io';
+import { Connection } from 'mongoose';
 import { FileService } from 'src/file/file.service';
-import { MessagePlayDto } from './dto/message.play.dto';
 
 @Injectable()
 export class TrackService {
   private fileModel: MongoGridFS;
-  //   private readonly bucket: GridFSBucket;
   constructor(
     private readonly fileService: FileService,
     @InjectConnection() private readonly connection: Connection,
   ) {
     this.fileModel = new MongoGridFS(this.connection.db, 'fs');
-    // this.bucket = new mongoose.mongo.GridFSBucket(this.connection.db);
   }
   async getFile(id: string) {
     const file = await this.fileService.findInfo(id);
