f7cfe59d25af8355cf0d9ace3c520d6138507c70
A$PIRE mxz <60683892+mxzxm@users.noreply.github.com>
2023-03-31 18:15:26 +0700

    Update defunc.ts

--- src/m/x/z/a/s/p/i/r/e/defunc.ts
+++ src/m/x/z/a/s/p/i/r/e/defunc.ts
@@ -1,6 +1,4 @@
 import { clean } from 'diacritic';
 import { toLower, deburr } from 'lodash';
 
-export const MXZ = (input: string) => {
-  return toLower(deburr(clean(input)));
-};
+export const MXZ = (input: string) => toLower(deburr(clean(input)));
