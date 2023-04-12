c496632c98bf1cf093ce9289279389fe0792e037
A$PIRE mxz <60683892+mxzxm@users.noreply.github.com>
2023-04-12 21:48:57 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { MxzModule } from './mxz/mxz.module';
 import { SELECTED } from './constants';
 
 (async () => {
-  const app = await NestFactory.create(MxzModule);
-  app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
-  app.enableCors({
+  const mxz = await NestFactory.create(MxzModule);
+  mxz.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
+  mxz.enableCors({
     origin: '*',
     credentials: true,
   });
-  await app.listen(3000);
-  SELECTED.UrlServer = await app.getUrl();
+  await mxz.listen(3000);
+  SELECTED.UrlServer = await mxz.getUrl();
 })();
