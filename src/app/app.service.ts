21cd0d7adc843d2b9eccabf7833f7679c8f5d1fb
user <user@710.local>
2023-06-30 22:07:00 +0700

    close

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,12 +1,7 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import {Injectable, Logger} from '@nestjs/common';
+import {Cron, CronExpression} from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
-  private readonly logger = new Logger(AppService.name);
 
-  @Cron(CronExpression.EVERY_10_MINUTES)
-  handleCron() {
-    this.logger.debug('Called when the current minute is 10');
-  }
 }
